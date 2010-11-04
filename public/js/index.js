6adbefb633b54046411b7307ddb2fe3ccc042cf6
Josef Richter <josef.richter@me.com>
2010-11-04 11:44:13 +0100

    changed homepage photos tag to hpsafe

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   
-	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=53456090@N02&tags=homepage&format=json&jsoncallback=?",
+	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=53456090@N02&tags=hpsafe&format=json&jsoncallback=?",
 	  function(data){
 			//console.log(data);
 	    $.each(data.items, function(i,item){
